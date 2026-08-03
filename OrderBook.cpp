904489577d9e6557ee951873c4ae05044d5c0bc4
Nico Chalazonitis <nickchalazonitis@gmail.com>
2026-08-03 13:29:20 +0300

    Implemented OrderBook addOrder + cancelOrder

---
+++ OrderBook.cpp
@@ -0,0 +1,70 @@
+#include "OrderBook.h"
+
+#include <algorithm>
+
+bool OrderBook::addOrder(Quantity quantity, Price price, Side side, OrderId& assignedId) {
+    
+    //checks
+    if (quantity <= 0 || price <= 0) {
+        return false;
+    }
+
+    if (side != Side::Buy && side != Side::Sell) {
+        return false;
+    }
+
+    Order order = {m_nextId, quantity, price, side};
+    assignedId = m_nextId;
+
+    //add to price map
+    if (side == Side::Buy) {
+        m_bids[price].push_back(m_nextId);
+    }
+    else {
+        m_asks[price].push_back(m_nextId);
+    }
+
+    m_idToOrder.insert({m_nextId, order});
+    m_nextId++;
+    return true;
+}
+
+bool OrderBook::cancelOrder(OrderId id) {
+    
+    auto orderIt = m_idToOrder.find(id);
+
+    if (orderIt == m_idToOrder.end()) {
+        return false;
+    }
+    Side side = orderIt->second.side;
+    Price price = orderIt->second.price;
+
+    //map side 
+    auto& sideMap = side == Side::Sell ? m_asks : m_bids;
+
+    //find in side maps
+    auto vectorOrderIt = sideMap.find(price);
+    if (vectorOrderIt != sideMap.end()) {
+        auto& priceVector = vectorOrderIt->second;
+        if (priceVector.empty()) {
+            return false;
+        }
+        else {
+            auto findIt = std::find(priceVector.begin(), priceVector.end(), id);
+            if (findIt != priceVector.end()) {
+                //here vector is found in the correct price
+                priceVector.erase(findIt);
+                if (priceVector.empty()) {
+                    sideMap.erase(price);
+                }
+                //erase order from idOrder map
+                m_idToOrder.erase(orderIt);
+                return true;
+            }
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
