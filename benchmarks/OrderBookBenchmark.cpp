59b0a230759a57556e6b45706666a5959cf7ab3a
Nico Chalazonitis <nickchalazonitis@gmail.com>
2026-08-17 00:07:26 +0300

    Add percentilePrint + main

--- benchmarks/OrderBookBenchmark.cpp
+++ benchmarks/OrderBookBenchmark.cpp
@@ -16,9 +16,20 @@ long long nearestRankPercentile(const std::vector<long long>& samples, double pe
     return samples[rank - 1];
 }
 
-// void percentilePrint() {
-//     return;
-// }
+void percentilePrint(std::vector<long long> samples) {
+
+    std::sort(samples.begin(), samples.end());
+
+    long long p50 = nearestRankPercentile(samples, 0.50);
+    long long p90 = nearestRankPercentile(samples, 0.90);
+    long long p99 = nearestRankPercentile(samples, 0.99);
+    long long p99_9 = nearestRankPercentile(samples, 0.999);
+
+    std::cout<<"p50 "<<p50<<'\n';
+    std::cout<<"p90 "<<p90<<'\n';
+    std::cout<<"p99 "<<p99<<'\n';
+    std::cout<<"p99.9 "<<p99_9<<'\n';
+}
 
 std::vector<long long> clockBase(size_t sampleCount) {
     std::vector<long long> samples;
@@ -155,7 +166,7 @@ std::vector<long long> cancelRandom(size_t sampleCount, size_t depth) {
     return samples;
 }
 
-std::vector<long long> mixedOrders(size_t sampleCount, size_t depth) {
+std::vector<long long> mixedOperations(size_t sampleCount, size_t depth) {
     OrderBook book;
     std::vector<long long> samples;
     samples.reserve(sampleCount);
@@ -218,118 +229,46 @@ int main() {
 
     std::cout<<"Matching Engine Latency Benchmark\n"; 
 
-    constexpr std::size_t sampleCount = 1'000'000;
+    const std::size_t sampleCount = 100'000;
+    std::vector<size_t> depths{0, 1, 100, 1'000, 10'000, 100'000};
+
+    //CLOCK
+    std::cout<<"Clock samples\n";
     std::vector<long long> clockSamples;
-    clockSamples.reserve(sampleCount);
-
-    for (std::size_t i = 0; i < sampleCount; ++i) {
-        const auto start{std::chrono::steady_clock::now()};
-        const auto finish{std::chrono::steady_clock::now()};
-        const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
-        const auto ns = elapsed.count();
-        clockSamples.push_back(ns);
-    }
-
-    std::sort(clockSamples.begin(), clockSamples.end());
-
-    std::cout<<clockSamples.size()<<'\n';
-    std::cout<<clockSamples[0]<<'\n';
-
-    long long p50 = nearestRankPercentile(clockSamples, 0.50);
-    long long p90 = nearestRankPercentile(clockSamples, 0.90);
-    long long p99 = nearestRankPercentile(clockSamples, 0.99);
-    long long p99_9 = nearestRankPercentile(clockSamples, 0.999);
-
-    std::cout<<"p50 "<<p50<<'\n';
-    std::cout<<"p90 "<<p90<<'\n';
-    std::cout<<"p99 "<<p99<<'\n';
-    std::cout<<"p99.9 "<<p99_9<<'\n';
-
-    //check one order
-    OrderBook book;
-    const auto start{std::chrono::steady_clock::now()};
-    std::optional<SubmissionResult> res = book.addOrder(100, 10'000, Side::Buy);
-    if (res == std::nullopt) return 1;
-    OrderId id = res->orderId;
-    if (!book.cancelOrder(id)) return 1;
-    const auto finish{std::chrono::steady_clock::now()};
-    const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
-    const auto ns = elapsed.count();
-    std::cout<<"Add and Cancel time "<<ns<<'\n';
+    clockSamples = clockBase(sampleCount);
+    percentilePrint(clockSamples);
     
-    //addOrder without cross
-    std::cout<<"Benchmark addOrder with no order being matched"<<'\n';
-    std::vector<long long> addSamples;
-    addSamples.reserve(sampleCount);
-
-    long long idCounter = 0;
-    for (std::size_t i = 0; i < sampleCount; ++i) {
-        const auto start{std::chrono::steady_clock::now()};
-        std::optional<SubmissionResult> res = book.addOrder(100, 10'000, Side::Buy);
-        const auto finish{std::chrono::steady_clock::now()};
-        if (res == std::nullopt) return 1;
-        idCounter += res->orderId;
-        const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
-        const auto ns = elapsed.count();
-        addSamples.push_back(ns);
+    //RESTING
+    for (size_t depth : depths) {
+        std::cout<<"addResting depth: "<<depth<<'\n';
+        percentilePrint(addResting(sampleCount, depth));
     }
-
-    std::sort(addSamples.begin(), addSamples.end());
-
-    std::cout<<addSamples.size()<<'\n';
-    std::cout<<addSamples[0]<<'\n';
-    std::cout<<"Aggregate id sum (prevents compiler dead code deletion) "<<idCounter<<'\n';
-
-    p50 = nearestRankPercentile(addSamples, 0.50);
-    p90 = nearestRankPercentile(addSamples, 0.90);
-    p99 = nearestRankPercentile(addSamples, 0.99);
-    p99_9 = nearestRankPercentile(addSamples, 0.999);
-
-    std::cout<<"p50 "<<p50<<'\n';
-    std::cout<<"p90 "<<p90<<'\n';
-    std::cout<<"p99 "<<p99<<'\n';
-    std::cout<<"p99.9 "<<p99_9<<'\n';
-
-    //MAKE BNNEW BOOK
-    
-
-    //add order with match
-    std::cout<<"Benchmark addOrder with nessesary matched"<<'\n';
-    std::vector<long long> addMatchSamples;
-    addMatchSamples.reserve(sampleCount);
-
-    for (std::size_t i = 0; i < sampleCount; ++i) {
-        std::optional<SubmissionResult> res = book.addOrder(100, 10'000, Side::Sell);
+    depths = {1, 100, 1'000, 10'000, 100'000};
+    //MATCHING
+    for (size_t depth : depths) {
+        std::cout<<"addMatching depth: "<<depth<<'\n';
+        percentilePrint(addMatching(sampleCount, depth));
     }
-
-    idCounter = 0;
-    for (std::size_t i = 0; i < sampleCount; ++i) {
-        const auto start{std::chrono::steady_clock::now()};
-        std::optional<SubmissionResult> res = book.addOrder(100, 10'000, Side::Buy);
-        const auto finish{std::chrono::steady_clock::now()};
-        if (res == std::nullopt) return 1;
-        idCounter += res->orderId;
-        const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
-        const auto ns = elapsed.count();
-        addMatchSamples.push_back(ns);
+    //CANCEL OLDEST
+    for (size_t depth : depths) {
+        std::cout<<"cancelOldest depth: "<<depth<<'\n';
+        percentilePrint(cancelOldest(sampleCount, depth));
     }
-
-    std::sort(addMatchSamples.begin(), addMatchSamples.end());
-
-    std::cout<<addMatchSamples.size()<<'\n';
-    std::cout<<addMatchSamples[0]<<'\n';
-    std::cout<<"Aggregate id sum (prevents compiler dead code deletion) "<<idCounter<<'\n';
-
-    p50 = nearestRankPercentile(addMatchSamples, 0.50);
-    p90 = nearestRankPercentile(addMatchSamples, 0.90);
-    p99 = nearestRankPercentile(addMatchSamples, 0.99);
-    p99_9 = nearestRankPercentile(addMatchSamples, 0.999);
-
-    std::cout<<"p50 "<<p50<<'\n';
-    std::cout<<"p90 "<<p90<<'\n';
-    std::cout<<"p99 "<<p99<<'\n';
-    std::cout<<"p99.9 "<<p99_9<<'\n';
-
+    //CANCEL NEWEST
+    for (size_t depth : depths) {
+        std::cout<<"cancelNewest depth: "<<depth<<'\n';
+        percentilePrint(cancelNewest(sampleCount, depth));
+    }
+    //CANCEL RANDOM
+    for (size_t depth : depths) {
+        std::cout<<"cancelRandom depth: "<<depth<<'\n';
+        percentilePrint(cancelRandom(sampleCount, depth));
+    }
+    //MIXED OPERATIONS
+    for (size_t depth : depths) {
+        std::cout<<"mixedOperations depth: "<<depth<<'\n';
+        percentilePrint(mixedOperations(sampleCount, depth));
+    }
 
     return 0;
 }
