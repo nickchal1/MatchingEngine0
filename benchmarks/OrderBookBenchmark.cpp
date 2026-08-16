93139086b82115b28233416427b97747cb210606
Nico Chalazonitis <nickchalazonitis@gmail.com>
2026-08-16 21:57:00 +0300

    Implement benchmark functions

--- benchmarks/OrderBookBenchmark.cpp
+++ benchmarks/OrderBookBenchmark.cpp
@@ -6,12 +6,148 @@
 #include <cmath>
 #include <algorithm>
 #include <optional>
+#include <cstdlib>
 
 long long nearestRankPercentile(const std::vector<long long>& clockSamples, double percentile) {
 
     long long rank = std::ceil(percentile * clockSamples.size());
     
     return clockSamples[rank - 1];
+}
+
+std::vector<long long> clockBase(size_t sampleCount) {
+    std::vector<long long> samples;
+    samples.reserve(sampleCount);
+    for (std::size_t i = 0; i < sampleCount; ++i) {
+        const auto start{std::chrono::steady_clock::now()};
+        const auto finish{std::chrono::steady_clock::now()};
+        const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
+        const auto ns = elapsed.count();
+        samples.push_back(ns);
+    }
+    return samples;
+}
+ 
+std::vector<long long> addResting(size_t sampleCount, size_t depth) {
+    OrderBook book;
+    std::vector<long long> samples;
+    samples.reserve(sampleCount);
+
+    for (std::size_t i = 0; i < depth; ++i) {
+        book.addOrder(100, 10'000, Side::Buy);
+    }
+    
+    for (std::size_t i = 0; i < sampleCount; ++i) {
+        const auto start{std::chrono::steady_clock::now()};
+        std::optional<SubmissionResult> res =  book.addOrder(100, 10'000, Side::Buy);
+        const auto finish{std::chrono::steady_clock::now()};
+        const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
+        const auto ns = elapsed.count();
+        samples.push_back(ns);
+
+        //untimed
+        book.cancelOrder(res->orderId);
+    }
+    return samples;
+}
+
+std::vector<long long> addMatching(size_t sampleCount, size_t depth) {
+    OrderBook book;
+    std::vector<long long> samples;
+    samples.reserve(sampleCount);
+
+    for (std::size_t i = 0; i < depth; ++i) {
+        book.addOrder(100, 10'000, Side::Buy);
+    }
+    
+    for (std::size_t i = 0; i < sampleCount; ++i) {
+        const auto start{std::chrono::steady_clock::now()};
+        book.addOrder(100, 10'000, Side::Sell);
+        const auto finish{std::chrono::steady_clock::now()};
+        const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
+        const auto ns = elapsed.count();
+        samples.push_back(ns);
+
+        //untimed
+        book.addOrder(100, 10'000, Side::Buy);
+    }
+    return samples;
+}
+
+std::vector<long long> cancelOldest(size_t sampleCount, size_t depth) {
+    OrderBook book;
+    std::vector<long long> samples;
+    samples.reserve(sampleCount);
+
+    for (std::size_t i = 0; i < depth; ++i) {
+        book.addOrder(100, 10'000, Side::Buy);
+    }
+
+    for (std::size_t i = 0; i < sampleCount; ++i) {
+        const auto start{std::chrono::steady_clock::now()};
+        book.cancelOrder(i + 1);
+        const auto finish{std::chrono::steady_clock::now()};
+        const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
+        const auto ns = elapsed.count();
+        samples.push_back(ns);
+        
+        //untimed
+        book.addOrder(100, 10'000, Side::Buy);
+    }
+    return samples;
+}
+
+std::vector<long long> cancelNewest(size_t sampleCount, size_t depth) {
+    OrderBook book;
+    std::vector<long long> samples;
+    samples.reserve(sampleCount);
+
+    for (std::size_t i = 0; i < depth; ++i) {
+        book.addOrder(100, 10'000, Side::Buy);
+    }
+
+    for (std::size_t i = 0; i < sampleCount; ++i) {
+        const auto start{std::chrono::steady_clock::now()};
+        book.cancelOrder(depth + i);
+        const auto finish{std::chrono::steady_clock::now()};
+        const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
+        const auto ns = elapsed.count();
+        samples.push_back(ns);
+        
+        //untimed
+        book.addOrder(100, 10'000, Side::Buy);
+    }
+    return samples;
+}
+
+std::vector<long long> cancelRandom(size_t sampleCount, size_t depth) {
+    OrderBook book;
+    std::vector<long long> samples;
+    samples.reserve(sampleCount);
+    std::vector<OrderId> validIds;
+
+    for (std::size_t i = 0; i < depth; ++i) {
+        std::optional<SubmissionResult> res = book.addOrder(100, 10'000, Side::Buy);
+        validIds.push_back(res->orderId);
+    }
+
+    srand(2029);
+    for (std::size_t i = 0; i < sampleCount; ++i) {
+        size_t randomIndex = rand() % depth;
+        const auto start{std::chrono::steady_clock::now()};
+        book.cancelOrder(validIds[randomIndex]);
+        const auto finish{std::chrono::steady_clock::now()};
+        const auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start);
+        const auto ns = elapsed.count();
+        
+        //untimed
+        samples.push_back(ns);
+        std::optional<SubmissionResult> res = book.addOrder(100, 10'000, Side::Buy);
+        std::swap(validIds[randomIndex], validIds[validIds.size() - 1]);
+        validIds.pop_back();
+        validIds.push_back(res->orderId);
+    }
+    return samples;
 }
 
 int main() {
@@ -91,6 +227,7 @@ int main() {
     std::cout<<"p99.9 "<<p99_9<<'\n';
 
     //MAKE BNNEW BOOK
+    
 
     //add order with match
     std::cout<<"Benchmark addOrder with nessesary matched"<<'\n';
